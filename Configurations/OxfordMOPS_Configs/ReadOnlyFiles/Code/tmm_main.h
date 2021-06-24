d8b0e074ab3b2f857df48646c4445a4024228efd
SophyOliver <50869860+SophyOliver@users.noreply.github.com>
2021-06-24 16:18:50 +0100

    Delete Configurations/OxfordMOPS_Configs/ReadOnlyFiles/Code directory

--- Configurations/OxfordMOPS_Configs/ReadOnlyFiles/Code/tmm_main.h
+++
@@ -1,6 +0,0 @@
-#define MAXNUMTRACERS 30
-
-extern PetscScalar deltaTClock, time0;
-extern PetscInt maxSteps, Iter0, writeSteps;
-extern PetscInt *gIndices, gLow, gHigh;
-extern PetscInt *gBCIndices, lBCSize, gBCSize, gbcLow, gbcHigh;
